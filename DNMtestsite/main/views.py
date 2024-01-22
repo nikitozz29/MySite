7f0fe4d208c291e61fd8733a9ddb39fbd5bb9adb
Nikita Dolgolenko <nikitozz29@yandex.ru>
2024-01-22 17:07:19 +0300

    Ошибка с css

--- DNMtestsite/main/views.py
+++ DNMtestsite/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def testtest(request):
+    return HttpResponse("<h3><i><b>ТЕСТОВАЯ строка</b></i></h3>")
+
